b9a2f692072bcff61aab0aa8780dc4b78635133e
jrferrera <jrferrera17@gmail.com>
2014-12-11 18:43:20 +0800

    Fixed style

--- sociagraph/settings.py
+++ sociagraph/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sociagraph',
-    'theme_matcher',
+    'key_information_extractor',
     'sentiment_analyzer',
 )
 
